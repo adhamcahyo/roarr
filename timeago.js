2fe43e67d2c2076adb5eb9de48019813afaa9ebb
adhamcahyo <127497339+adhamcahyo@users.noreply.github.com>
2024-09-15 20:25:35 +0700

    8

--- timeago.js
+++ timeago.js
@@ -1,15 +1,91 @@
-var _0xabc1 = ["\x66\x75\x6E\x63\x74\x69\x6F\x6E", "\x74\x79\x70\x65\x6F\x66", "\x64\x65\x66\x69\x6E\x65", "\x61\x6D\x64", "\x66\x61\x63\x74\x6F\x72\x79", "\x6A\x71\x75\x65\x72\x79", "\x65\x78\x70\x6F\x72\x74\x73", "\x6D\x6F\x64\x75\x6C\x65"];
-(function (_0x1234) {
-    if (typeof define === _0xabc1[0] && define.amd) {
-        define([_0xabc1[1]], function (_0x5678) {
-            return _0x5678;
-        });
-    } else if (typeof module === _0xabc1[2] && module.exports) {
-        module.exports = factory(require(_0xabc1[1]));
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jquery'));
     } else {
-        root[_0xabc1[3]] = factory(root[_0xabc1[3]]);
+        root.timeago = factory(root.jQuery);
     }
-}());
+}(this, function ($) {
+    var $el;
+    var locales = {};
+
+    function timeago() {
+        if (!$el) {
+            $el = $('<div></div>').hide().appendTo(document.body);
+        }
+        return $el;
+    }
+
+    function getTime() {
+        return Math.floor(Date.now() / 1000);
+    }
+
+    function getLocale(locale) {
+        if (locales[locale]) {
+            return locales[locale];
+        }
+        return locales.en;
+    }
+
+    function timeagoLocale(locale, obj) {
+        locales[locale] = obj;
+    }
+
+    function inWords(time) {
+        var prefix = '';
+        var suffix = 'ago';
+        var seconds = time / 1000;
+        var interval = Math.floor(seconds / 31536000);
+        if (interval > 1) {
+            return interval + ' years';
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + ' months';
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + ' days';
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + ' hours';
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + ' minutes';
+        }
+        return Math.floor(seconds) + ' seconds';
+    }
+
+    function updateTime() {
+        var elements = document.querySelectorAll('abbr.timeago');
+        elements.forEach(function (element) {
+            var timestamp = element.getAttribute('title');
+            var date = new Date(timestamp);
+            var now = getTime();
+            var then = Math.floor(date.getTime() / 1000);
+            var time = (now - then) * 1000;
+            var locale = getLocale(element.getAttribute('data-locale') || 'en');
+            var timeString = inWords(time);
+            element.innerHTML = timeString;
+        });
+    }
+
+    $.fn.timeago = function () {
+        updateTime();
+        setInterval(updateTime, 60000);
+        return this;
+    };
+
+    return {
+        locale: timeagoLocale,
+        settings: {
+            locale: 'en',
+        }
+    };
+}));
 
 
 //hshshs kasian arlina ketauan yya ngumpetin script redirect ke template blog gratisan wakawokaokok  creditnya di delete dah ga bisa redirect deh orang gw dah bikin time ago sendiri awkawkaok
